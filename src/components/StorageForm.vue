155511ecc0e32bbe19150b0d3bdbd5dfb4f22f93
Wojciech Krawiec <wojciech.krawiec-ext@hexagon.com>
2023-03-27 16:17:24 +0200

    event added

--- src/components/StorageForm.vue
+++ src/components/StorageForm.vue
@@ -4,7 +4,7 @@
       <form @submit.prevent="cookieSaveHandler">
         <label>Cookie Input</label>
         <input v-model="cookieInput" />
-        <button type="submt" >Save</button>
+        <button type="submt">Save</button>
       </form>
     </div>
     <div>
@@ -28,6 +28,7 @@
         <div>sessionStore item: {{ sessionStorageVal }}</div>
       </div>
     </div>
+    <button type="button" @click="emitHandler">Emitt event</button>
   </div>
 </template>
 <script setup>
@@ -51,12 +52,12 @@ const cookieSaveHandler = () => {
 };
 
 const localStorageHandler = () => {
-  localStorage.setItem(itemKey, localStorageInput.value)
+  localStorage.setItem(itemKey, localStorageInput.value);
   getStoredValues();
 };
 
 const sessionStorageHandler = () => {
-  sessionStorage.setItem(itemKey, sessionStorageInput.value)
+  sessionStorage.setItem(itemKey, sessionStorageInput.value);
   getStoredValues();
 };
 
@@ -64,6 +65,16 @@ const getStoredValues = () => {
   cookieVal.value = cookies.get(itemKey);
   localStorageVal.value = localStorage.getItem(itemKey) || "none";
   sessionStorageVal.value = sessionStorage.getItem(itemKey) || "none";
+};
+
+const emitHandler = () => {
+  const myEvent = new CustomEvent("myevent", {
+    detail: {},
+    bubbles: true,
+    cancelable: true,
+    composed: false,
+  });
+  document.dispatchEvent(myEvent);
 };
 
 onMounted(() => {
