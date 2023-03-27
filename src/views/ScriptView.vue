5bc524bd650fd08c4c183d41a6a80965572a3f60
Wojciech Krawiec <wojciech.krawiec-ext@hexagon.com>
2023-03-27 23:23:02 +0200

    styles added

--- src/views/ScriptView.vue
+++ src/views/ScriptView.vue
@@ -14,9 +14,19 @@ const addScript = (url) => {
   document.head.appendChild(script);
 };
 
-onMounted(async () => {
-  const url = process.env.VUE_APP_SCRIPT_SRC;
+const addStyle = (url) => {
+  var newstyle = document.createElement("link");
+  newstyle.setAttribute("rel", "stylesheet");
+  newstyle.setAttribute("type", "text/css");
+  newstyle.setAttribute("href", url);
+   document.head.appendChild(newstyle);
+};
 
-  addScript(url);
+onMounted(async () => {
+  const scriptUrl = process.env.VUE_APP_SCRIPT_SRC;
+  const styleUrl = process.env.VUE_APP_STYLE_SRC;
+
+  addScript(scriptUrl);
+  addStyle(styleUrl);
 });
 </script>
