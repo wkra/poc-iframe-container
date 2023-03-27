80236e5a6ad51ce1e174bd0af0307e7eabe49bbc
Wojciech Krawiec <wojciech.krawiec-ext@hexagon.com>
2023-03-27 23:06:24 +0200

    popmessage added

--- src/components/CustomEvent.vue
+++ src/components/CustomEvent.vue
@@ -3,6 +3,7 @@
     <hr />
     <div>Test parent / child comunication</div>
     <button type="button" @click="emitHandler">Emit event do child</button>
+    <button type="button" @click="emitPostMessage">Emit postMessage</button>
   </div>
 </template>
 <script setup>
@@ -26,11 +27,32 @@ const eventHandler = () => {
   alert("I am container. Event from child reveived");
 };
 
+const emitPostMessage = () => {
+  window.parent.postMessage(
+    { myCustomParent: true, message: "message from parent" },
+    "*"
+  );
+};
+
+const popMessageHandler = (event) => {
+  const urls = [
+    "http://localhost:8081",
+    "http://localhost:8080",
+    "https://wkra.github.io/poc-iframe-list",
+    "https://wkra.github.io/poc-iframe-container",
+  ];
+
+  if (!urls.includes(event.origin)) return;
+  if (event.data.myCustomChild) alert(event.data.message);
+};
+
 onMounted(() => {
   document.addEventListener("childevent", eventHandler);
+  window.addEventListener("message", popMessageHandler);
 });
 
 onUnmounted(() => {
   document.removeEventListener("childevent", eventHandler);
+  window.removeEventListener("message", popMessageHandler);
 });
 </script>
